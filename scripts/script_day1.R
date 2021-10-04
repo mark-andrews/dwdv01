a5a78366722163a08783c4b293ca126594a0e64c
Mark Andrews <mjandrews.org@gmail.com>
2021-10-04 21:44:09 +0100

    Update script

--- scripts/script_day1.R
+++ scripts/script_day1.R
@@ -9,7 +9,8 @@ blp_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/dwdv01/master
 
 # verbs
 
-# select 
+# select
+# relocate
 # rename
 # slice
 # filter
@@ -71,3 +72,148 @@ rename_with(blp_df, toupper)
 rename_with(blp_df, 
             ~str_replace(., 'rt', 'reaction_time'),
             matches('^rt|rt$'))
+
+
+# slice rows by index -----------------------------------------------------
+
+slice(blp_df, 10:20)
+blp_df[10:20,]
+slice(blp_df, 3, 5, 10, 20)
+slice(blp_df, 3, 5, 10:15)
+slice(blp_df, -1)
+slice(blp_df, 1:10)
+slice(blp_df, n())
+slice(blp_df, (n()-9):n())
+
+# filtering rows with filter
+filter(blp_df, lex == 'W')
+filter(blp_df, lex == 'W', resp == 'W')
+filter(blp_df, rt < 500)
+filter(blp_df, rt < 500 & resp == 'W')
+filter(blp_df, rt < 500, resp == 'W')
+filter(blp_df, rt < 500 | resp == 'W') # rt < 500 OR resp == 'W'
+filter(blp_df, !(rt < 500) | resp == 'W' & lex == 'W')
+
+filter(blp_df, if_any(rt:rt.raw, ~ . < 500))
+filter(blp_df, if_all(rt:rt.raw, ~ . < 500))
+
+filter(blp_df, if_all(rt:rt.raw, ~ . < median(., na.rm = T)))
+filter(blp_df, if_all(where(is.numeric),
+                      ~ . < median(., na.rm = T)))
+
+
+# create new variables with mutate ----------------------------------------
+
+mutate(blp_df, accuracy = lex == resp)
+
+mutate(blp_df, 
+       accuracy = lex == resp,
+       word_length = str_length(spell)
+)
+
+mutate(blp_df, lex == resp)
+
+mutate(blp_df, lex = as.factor(lex))
+
+mutate(blp_df,
+       lex = as.factor(lex),
+       spell = as.factor(spell),
+       resp = as.factor(resp)
+)
+
+mutate(blp_df,
+       across(where(is.character), as.factor)
+)
+
+
+mutate(blp_df,
+       across(lex:resp, as.factor)
+)
+
+mutate(blp_df,
+       across(c(lex, spell, resp), as.factor)
+)
+
+
+mutate(blp_df,
+       across(where(is.numeric), ~as.vector(scale(.)))
+)
+
+# this: ~as.vector(scale(.))
+# is like this
+re_scale <- function(x){
+  as.vector(scale(x))
+}
+
+mutate(blp_df,
+       across(where(is.numeric), re_scale)
+)
+
+mutate(blp_df,
+       across(where(is.numeric), function(x){as.vector(scale(x))})
+)
+
+mutate(blp_df,
+       across(where(is.numeric), ~as.vector(scale(.)))
+)
+
+
+mutate(blp_df,
+       lex = recode(lex, 'W' = 'word', 'N' = 'nonword')
+)
+
+mutate(blp_df,
+       lex = recode(lex, 'W' = 'word', 'N' = 'nonword'),
+       resp = recode(resp, 'W' = 'word', 'N' = 'nonword')
+)
+
+mutate(blp_df,
+       across(c(lex, resp),
+              ~recode(., 'W' = 'word', 'N' = 'nonword')
+       )
+)
+
+my_recoding_f <- function(x){
+  recode(x, 'W' = 'word', 'N' = 'nonword')
+}
+
+mutate(blp_df,
+       across(c(lex, resp), my_recoding_f)
+)
+
+mutate(blp_df,
+       rt_speed = if_else(rt > 900, 'slow', 'fast')
+)
+
+mutate(blp_df,
+       rt_speed = case_when(
+         rt > 900 ~ "slow",
+         rt < 600 ~ "fast",
+         TRUE ~ "medium"
+       )
+)
+mutate(blp_df, 
+       accuracy = lex == resp) %>% select(-c(lex, resp))
+
+mutate(blp_df, 
+       accuracy = lex == resp,
+       word_length = str_length(spell)
+)
+
+transmute(blp_df, 
+       accuracy = lex == resp,
+       word_length = str_length(spell)
+)
+
+
+# sorting with arrange ----------------------------------------------------
+
+arrange(blp_df, rt)
+arrange(blp_df, desc(rt))
+arrange(blp_df, participant, rt)
+arrange(blp_df, spell)
+
+
+# pipelines with %>% ------------------------------------------------------
+
+%>% 
